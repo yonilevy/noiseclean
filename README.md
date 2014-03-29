5854a71f915291904ed46d435afc2b0abf34a5a5
yonilevy <yonilevy@gmail.com>
2014-03-29 15:28:19 +0300

    add support for cleaning audio files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 noiseclean
 ==========
 
-A simple bash script to remove audio noise from a video file.
+A simple bash script to remove audio noise from audio and video files.
 
 Based on: http://www.zoharbabin.com/how-to-do-noise-reduction-using-ffmpeg-and-sox/
--- noiseclean.sh
+++ noiseclean.sh
@@ -26,6 +26,19 @@ then
     esac
 fi
 
+inBasename=$(basename "$1")
+inExt="${inBasename##*.}"
+
+isVideoStr=`ffprobe -v warning -show_streams "$1" | grep codec_type=video`
+if [[ ! -z $isVideoStr ]]
+then
+    isVideo=1
+    echo "Detected '$inBasename' as a video file"
+else
+    isVideo=0
+    echo "Detected '$inBasename' as an audio file"
+fi
+
 read -p "Sample noise start time [00:00:00]: " sampleStart
 if [[ -z $sampleStart ]] ; then sampleStart="00:00:00"; fi
 read -p "Sample noise end time [00:00:00.500]: " sampleEnd
@@ -33,8 +46,6 @@ if [[ -z $sampleEnd ]] ; then sampleEnd="00:00:00.500"; fi
 read -p "Noise reduction amount [0.21]: " sensitivity
 if [[ -z $sensitivity ]] ; then sensitivity="0.21"; fi
 
-inBasename=$(basename "$1")
-inExt="${inBasename##*.}"
 
 tmpVidFile="/tmp/noiseclean_tmpvid.$inExt"
 tmpAudFile="/tmp/noiseclean_tmpaud.wav"
@@ -44,12 +55,18 @@ tmpAudCleanFile="/tmp/noiseclean_tmpaud-clean.wav"
 
 echo "Cleaning noise on '$1'..."
 
-ffmpeg -v warning -y -i "$1" -qscale:v 0 -an "$tmpVidFile"
-ffmpeg -v warning -y -i "$1" -qscale:a 0 "$tmpAudFile"
+if [ $isVideo -eq "1" ]; then
+    ffmpeg -v warning -y -i "$1" -qscale:v 0 -an "$tmpVidFile"
+    ffmpeg -v warning -y -i "$1" -qscale:a 0 "$tmpAudFile"
+fi
 ffmpeg -v warning -y -i "$1" -vn -ss "$sampleStart" -t "$sampleEnd" "$noiseAudFile"
 sox "$noiseAudFile" -n noiseprof "$noiseProfFile"
 sox "$tmpAudFile" "$tmpAudCleanFile" noisered "$noiseProfFile" "$sensitivity"
-ffmpeg -v warning -y -i "$tmpAudCleanFile" -i "$tmpVidFile" -qscale:v 0 -qscale:a 0 "$2"
+if [ $isVideo -eq "1" ]; then
+    ffmpeg -v warning -y -i "$tmpAudCleanFile" -i "$tmpVidFile" -qscale:v 0 -qscale:a 0 "$2"
+else
+    cp "$tmpAudCleanFile" "$2"
+fi
 
 echo "Done"
 
